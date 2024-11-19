6536945ff98cc907e4a31a594635d06c8ebb0152
Jemo <jamesshisia6@gmail.com>
2024-11-19 18:00:50 +0300

    github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enable static export
+  // basePath: '/chrispo_web_Portfolio', // Replace with your repository name
+  trailingSlash: true, // Ensures paths end with "/"
 };
 
 export default nextConfig;
